b8581cedaa4511f52b605600292367a615546f5e
PaawanSofTech <96773581+PaawanSofTech@users.noreply.github.com>
2022-09-05 22:20:26 +0530

    Add files via upload

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "example@example.net";
+$address = "animemines2004@gmail.com";
 
 
 // Configuration option.
